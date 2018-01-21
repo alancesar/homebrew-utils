86d9fe826d869bf00fc7fc78421a6bba0bff7ce4
Alan Cesar Elias <alancesarelias@gmail.com>
2018-01-21 11:51:09 -0200

    New ABV formula

--- calculators/abvCalculator.js
+++ calculators/abvCalculator.js
@@ -1,9 +1,13 @@
 module.exports = ({ og, fg }) => {
-  const abv = (og.sg.value - fg.sg.value) * 1.3125;
-  const attenuation = (og.sg.value - fg.sg.value) / (og.sg.value - 1);
+  const ogInSg = og.sg.value;
+  const fgInSg = fg.sg.value;
+  const abv = (((76.08 * (ogInSg - fgInSg)) / (1.775 - ogInSg)) * (fgInSg / 0.794)) / 100;
+  const abw = abv * 0.8;
+  const attenuation = (ogInSg - fgInSg) / (ogInSg - 1);
 
   return {
     abv,
+    abw,
     attenuation,
   };
 };
--- calculators/abvCalculator.spec.js
+++ calculators/abvCalculator.spec.js
@@ -4,13 +4,14 @@ const { abv } = require('./calculators');
 const { Density } = require('../units/units');
 
 describe('ABV Calculator', () => {
-  it('total ABV and Attenuation', () => {
+  it('total ABV, ABW and Attenuation', () => {
     const params = {
       og: new Density(1.042),
       fg: new Density(1.008),
     };
     const result = abv(params);
-    expect(result.abv).to.be.closeTo(0.044625, 0.0001);
+    expect(result.abv).to.be.closeTo(0.0448, 0.0001);
+    expect(result.abw).to.be.closeTo(0.0358, 0.0001);
     expect(result.attenuation).to.be.closeTo(0.8095, 0.0001);
   });
 });
