b714dbe72e2de12c2a10e1d16ace45b0ca677bae
Alan Cesar Elias <alan.elias@matera.com>
2018-03-13 16:21:06 -0300

    Object builder fix

--- utils/builder.js
+++ utils/builder.js
@@ -1,7 +1,7 @@
 module.exports = (prototype, properties = {}, extra = {}) => {
   if (typeof extra === 'function') {
-    return Object.assign(prototype, properties, extra(properties));
+    return Object.assign(Object.create(prototype), properties, extra(properties));
   }
 
-  return Object.assign(prototype, properties, extra);
+  return Object.assign(Object.create(prototype), properties, extra);
 };
